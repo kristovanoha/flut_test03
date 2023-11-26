998bd2a12e787f30d7eced4ae962ee58723786c5
Jiri Hegar <jiri.hegar@trustlab.cz>
2023-11-26 13:40:34 +0100

    krab

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,8 @@ import 'package:flutter_test/flutter_test.dart';
 //NAVIC POKUS
 import 'package:flut_test03/main.dart';
 //dsfsfad DDsdfsdf  VETEV POKUS sssss    AAAAAAAA
+
+//TERROR
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
