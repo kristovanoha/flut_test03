f2c256f0fd46f497b52106a5669c602cec0fb936
Jiri Hegar <jiri.hegar@trustlab.cz>
2023-11-26 12:45:09 +0100

    za devem

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flut_test03/main.dart';
-//vetev DEV sss
+//vetev DEV sss   ZA devem
+//TOHLE je navis
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
