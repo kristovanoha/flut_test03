db92a0bc6ddc20614f8e29b3b6e734ead58a4ed8
Jiri Hegar <jiri.hegar@trustlab.cz>
2023-11-27 19:16:40 +0100

    drrrd

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flut_test03/main.dart';
 //dsfsfad  MASTER
+
+
+//DRUHA featura  cococ 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
